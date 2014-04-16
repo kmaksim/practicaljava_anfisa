5a52a50818a41fa80437f554ae70090c1721e29a
anfisakho <anfisakho@icloud.com>
2014-04-16 15:36:35 +0400

    Added SwingWorker test

---
+++ unit11_anfisa/src/NewsReader2.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Scanner;
+
+import javax.swing.JTextArea;
+import javax.swing.SwingWorker;
+
+/*
+ * File Reader using Scanner to test SwingWorker
+ */
+public class NewsReader2 extends SwingWorker<String, String> {
+  String fileContents = "";
+  File fileName;
+
+  public NewsReader2(File file) {
+    this.fileName = file;
+  }
+
+  @Override
+  protected String doInBackground() throws Exception {
+
+    if (!fileName.exists() || !fileName.canRead()) {
+      System.out.println("Can't read the file");
+      return "Can't read the file";
+    }
+    String line;
+    try {
+      Reader ir = new InputStreamReader(new FileInputStream(fileName));
+
+      System.out.println("Reading file: " + fileName.getName());
+      BufferedReader in = new BufferedReader(ir);
+       
+        while ((line = in.readLine()) != null) {
+       
+          System.out.println("Line: " + line);
+          fileContents = fileContents + line + "\n";
+        }
+      } catch (FileNotFoundException e) {
+        System.out.println("File Disappeared");
+      }
+    return fileContents;
+  }
+
+  @Override
+  protected void done() {
+    if (isCancelled())
+      System.out.print("Cancelled !");
+    else
+      // SwingWorkerTest.textArea1.append("Done !");
+      System.out.println("Task done !");
+
+  }
+}
---
+++ unit11_anfisa/src/SwingWorkerTest2.java
@@ -0,0 +1,107 @@
+
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.io.File;
+
+
+public class SwingWorkerTest2 extends JFrame implements ActionListener {
+
+  /**
+   * Read files in GUI with SwingWorker help
+   */
+  
+  JButton buttonRead;
+  final NewsReader2 task1;
+  final NewsReader2 task2;
+  public static JTextArea textArea1;
+  public static JTextArea textArea2;
+  
+  SwingWorkerTest2() {
+    
+    String text = "File contents placeholder";
+    
+    textArea1 = new JTextArea(text, 5, 10);
+    
+    textArea1.setPreferredSize(new Dimension(100, 100));
+    
+    textArea2 = new JTextArea(text, 5, 10);
+    
+    textArea2.setPreferredSize(new Dimension(100, 100));
+    
+    JScrollPane scrollPane1 = new JScrollPane(textArea1,
+        JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+        JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+    
+    JScrollPane scrollPane2 = new JScrollPane(textArea2,
+            JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+            JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+    
+    textArea1.setLineWrap(true);
+    
+    textArea2.setLineWrap(true);
+
+    buttonRead = new JButton("Read Files");
+    
+    JPanel panel = new JPanel();
+    
+    panel.setLayout(new FlowLayout());
+        
+    panel.add(scrollPane1);
+    panel.add(scrollPane2); 
+    panel.add(buttonRead);
+    
+    //create border 
+    TitledBorder titleBorder = BorderFactory.createTitledBorder("File Reader");
+    panel.setBorder(titleBorder);
+    
+    add(panel);
+
+    buttonRead.addActionListener(this);
+       
+    task1 = new NewsReader2(new File("testText1.txt"));
+    task2 = new NewsReader2(new File("testText2.txt"));
+    
+    // Define, instantiate and register a WindowAdapter
+    // to process windowClosing Event of this frame
+
+    this.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        System.out.println("Good bye!");
+  	    System.exit(0);
+        }
+      });
+  }
+
+
+  public void actionPerformed(ActionEvent evt) {
+    Object source = evt.getSource();
+    
+    if (source == buttonRead ){
+      // button processing
+      try{
+         
+        task1.execute();
+        task2.execute();
+        
+        String res1 = task1.get();
+        String res2 = task2.get();
+        
+        textArea1.setText(res1);
+        textArea2.setText(res2);
+               
+       } catch (Exception e){
+           e.printStackTrace();
+        }
+     }    
+  }
+  
+  public static void main(String args[]){
+    SwingWorkerTest2 myFrame = new SwingWorkerTest2(); 
+
+    myFrame.setSize(350,200);
+    myFrame.setVisible(true);
+  }
+}
